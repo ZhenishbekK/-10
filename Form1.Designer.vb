1680abc6531cb624ad67bd2f73051693b383dbde
ZhenishbekK <manty006@gmail.com>
2025-05-12 12:18:51 +0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dtpDate = New System.Windows.Forms.DateTimePicker()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.txtCases = New System.Windows.Forms.TextBox()
+        Me.txtDisease = New System.Windows.Forms.TextBox()
+        Me.txtDistrict = New System.Windows.Forms.TextBox()
+        Me.txtRegion = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(105, 239)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(109, 13)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "Количество случаев"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(105, 200)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(74, 13)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "Заболевание"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(105, 145)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Район"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(105, 106)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Регион"
+        '
+        'dtpDate
+        '
+        Me.dtpDate.Location = New System.Drawing.Point(495, 268)
+        Me.dtpDate.Name = "dtpDate"
+        Me.dtpDate.Size = New System.Drawing.Size(200, 20)
+        Me.dtpDate.TabIndex = 15
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(542, 327)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 14
+        Me.btnSave.Text = "Сохранить"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'txtCases
+        '
+        Me.txtCases.Location = New System.Drawing.Point(231, 232)
+        Me.txtCases.Name = "txtCases"
+        Me.txtCases.Size = New System.Drawing.Size(100, 20)
+        Me.txtCases.TabIndex = 13
+        '
+        'txtDisease
+        '
+        Me.txtDisease.Location = New System.Drawing.Point(231, 193)
+        Me.txtDisease.Name = "txtDisease"
+        Me.txtDisease.Size = New System.Drawing.Size(100, 20)
+        Me.txtDisease.TabIndex = 12
+        '
+        'txtDistrict
+        '
+        Me.txtDistrict.Location = New System.Drawing.Point(231, 145)
+        Me.txtDistrict.Name = "txtDistrict"
+        Me.txtDistrict.Size = New System.Drawing.Size(100, 20)
+        Me.txtDistrict.TabIndex = 11
+        '
+        'txtRegion
+        '
+        Me.txtRegion.Location = New System.Drawing.Point(231, 100)
+        Me.txtRegion.Name = "txtRegion"
+        Me.txtRegion.Size = New System.Drawing.Size(100, 20)
+        Me.txtRegion.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dtpDate)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.txtCases)
+        Me.Controls.Add(Me.txtDisease)
+        Me.Controls.Add(Me.txtDistrict)
+        Me.Controls.Add(Me.txtRegion)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents dtpDate As DateTimePicker
+    Friend WithEvents btnSave As Button
+    Friend WithEvents txtCases As TextBox
+    Friend WithEvents txtDisease As TextBox
+    Friend WithEvents txtDistrict As TextBox
+    Friend WithEvents txtRegion As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    ' Строка подключения к базе данных
+    Dim connectionString As String = "Server=172.20.10.7;Database=InfectionDB;Integrated Security=True;"
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim region As String = txtRegion.Text
+        Dim district As String = txtDistrict.Text
+        Dim disease As String = txtDisease.Text
+        Dim cases As Integer = Integer.Parse(txtCases.Text)
+        Dim reportDate As DateTime = dtpDate.Value
+
+        ' Запрос для вставки данных
+        Dim query As String = "INSERT INTO InfectionData (Region, District, Disease, Cases, ReportDate) VALUES (@region, @district, @disease, @cases, @date)"
+
+        ' Используем блок Using для корректного управления соединением
+        Using connection As New SqlConnection(connectionString)
+            Try
+                ' Открытие соединения
+                connection.Open()
+
+                ' Создание команды
+                Using command As New SqlCommand(query, connection)
+                    ' Добавление параметров
+                    command.Parameters.AddWithValue("@region", region)
+                    command.Parameters.AddWithValue("@district", district)
+                    command.Parameters.AddWithValue("@disease", disease)
+                    command.Parameters.AddWithValue("@cases", cases)
+                    command.Parameters.AddWithValue("@date", reportDate)
+
+                    ' Выполнение команды
+                    command.ExecuteNonQuery()
+                End Using
+
+                ' Сообщение об успешном сохранении данных
+                MessageBox.Show("Данные успешно сохранены.")
+            Catch ex As Exception
+                ' Обработка ошибок
+                MessageBox.Show("Ошибка при сохранении данных: " & ex.Message)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Здесь можно добавить код для инициализации формы, если нужно
+    End Sub
+End Class
